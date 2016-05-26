da3047b73fcf3f5def236cea114c97014b01452c
Mikko Haapoja <me@mikkoh.com>
2016-05-26 10:31:27 -0400

    Fixed regex to allow for npm script shortcuts

--- test/echoProcessEnv.js
+++ test/echoProcessEnv.js
@@ -4,6 +4,6 @@ var fs = require('fs');
 
 var parsedArgs = minimist(process.argv.slice(2));
 var file = parsedArgs._[ 0 ];
-var contents = parsedArgs._[ 1 ];
+var contents = 'hello ' + process.env.SCRIPT;
 
 fs.writeFileSync(file, contents, 'utf8');
--- test/index.js
+++ test/index.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 var path = require('path');
 
 test('no variables', testFile.bind(undefined, 'test-no-var', 'hello test-no-var'));
-test('script name', testFile.bind(undefined, 'test-script-name', 'hello test-script-name'));
 test('variables', testFile.bind(undefined, 'test-vars', 'hello test-vars and world'));
 test('arguments', testFile.bind(undefined, 'test-args', 'hello test-args and mars'));
 test('pipe', testFile.bind(undefined, 'test-pipe', 'hello test-pipe and Canada'));
+test('npm script shortcut', testFile.bind(undefined, 'test-npm-shortcut', 'hello test-npm-shortcut'));
+test('use process.env', testFile.bind(undefined, 'test-read-env', 'hello test-read-env'));
 
 function testFile(file, expectedOut, t) {
   var out = fs.readFileSync(
